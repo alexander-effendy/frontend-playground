611f99bdd6aa4208d23a5b39552015488ecff08e
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-09-03 10:39:21 +1000

    setup: React TypeScript and Vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import Pages from './Pages';
+function App() {
+  return (
+    <BrowserRouter>
+      <Pages />
+    </BrowserRouter>
+  )
+}
+
+export default App
